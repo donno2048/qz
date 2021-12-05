301b88ad0165918d9678c9a550867f53a9153bf4
Elisha Hollander <just4now666666@gmail.com>
2021-12-05 23:40:02 +0200

    add `url`

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 setup(
     name='qz',
-    version='1',
+    version='2',
     description='Print "Hello, World!"',
+    url='https://github.com/donno2048/qz',
     packages=find_packages(),
     license='MIT',
     author='Elisha Hollander'
